36f47a3bdc40c292f4f7e65013948160ad9ad36b
daniloamaralr <daniloamaralr@gmail.com>
2021-03-13 18:10:50 -0300

    basic components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
